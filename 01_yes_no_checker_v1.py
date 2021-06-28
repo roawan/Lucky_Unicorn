ab8c7e66242d4b7ee2cadf0362670c9874eec807
roawan <woods.rowan@awatapu.school.nz>
2021-06-28 12:11:25 +1200

    Initial commit of yes no checker v1. added multiple acceptable answers

---
+++ 01_yes_no_checker_v1.py
@@ -0,0 +1,15 @@
+# ask user if they've played before
+show_instructions = input("Have you played before").strip().lower()
+# if they say yes, continue program
+if show_instructions == "y":
+    print("program continues")
+if show_instructions == "yes":
+    print("program continues")
+# if no, display instructions then continue
+elif show_instructions == "no":
+    print("show instructions")
+elif show_instructions == "n":
+    print("show instructions")
+# unexpected response
+else:
+    print("Please answer yes or no")
