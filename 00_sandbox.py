a04e4629e5bfece9ea64757bf5599dad5f984806
roawan <woods.rowan@awatapu.school.nz>
2021-07-05 13:15:59 +1200

    Updated and added a print function

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -2,5 +2,11 @@
 number = int(input("Choose a number"))
 # Multiply it by 5
 times_five = number * 5
+
+
+answer = "{} times 5 is equal to " \
+         "{}" .format(number, times_five)
+
+
 # Output the result
-print(times_five)
+print(answer)
