01e00b3973ac94ab196e54b9e9d86dba0a91e9b4
roawan <woods.rowan@awatapu.school.nz>
2021-07-26 12:14:44 +1200

    refined code

--- 01_yes_no_checker_v1.py
+++ 01_yes_no_checker_v1.py
@@ -8,18 +8,3 @@ if show_instructions == "y" or show_instructions == "yes":
 # if no, display instructions then continue
 elif show_instructions == "no" or show_instructions == "n":
     print("show instructions")
-
-=======
-if show_instructions == "y":
-    print("program continues")
-if show_instructions == "yes":
-    print("program continues")
-# if no, display instructions then continue
-elif show_instructions == "no":
-    print("show instructions")
-elif show_instructions == "n":
-    print("show instructions")
->>>>>>> origin/master
-# unexpected response
-else:
-    print("Please answer yes or no")
