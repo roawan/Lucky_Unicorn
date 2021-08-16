01735a7779f2595470e755e60eec592fc3a81c0f
roawan <86275735+roawan@users.noreply.github.com>
2021-08-16 12:38:22 +1200

    removed 2 elifs and condensed to else
    + video code

---
+++ 01_token_generator_v4.py
@@ -0,0 +1,22 @@
+import random
+
+tokens = ["unicorn", "donkey", "donkey", "donkey", "zebra", "horse", "horse", "horse" "zebra", "zebra"]
+STARTING_BALANCE = 100
+
+
+balance = STARTING_BALANCE
+for i in range(0, 500):
+    chosen = random.choice(tokens)
+
+
+    if chosen == "unicorn":
+        balance += 4
+    elif chosen == "donkey":
+        balance -= 1
+    else:
+        balance -= 0.5
+
+
+print()
+print("Starting Balance: ${:.2f}".format(STARTING_BALANCE))
+print("Final Balance: ${:.2f}".format(balance))
