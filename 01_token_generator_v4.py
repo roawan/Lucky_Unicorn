1af7c5ec43c5eb9de9d95884da0b92a5e4dd4eb3
roawan <86275735+roawan@users.noreply.github.com>
2021-08-16 12:54:52 +1200

    efficiency + rigging and downscaling

--- 01_token_generator_v4.py
+++ 01_token_generator_v4.py
@@ -5,17 +5,24 @@ STARTING_BALANCE = 100
 
 
 balance = STARTING_BALANCE
-for i in range(0, 500):
-    chosen = random.choice(tokens)
+for i in range(0, 10):
+    chosen_num = random.randint(1, 100)
 
 
-    if chosen == "unicorn":
+
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
         balance += 4
-    elif chosen == "donkey":
+    elif 6 <= chosen_num <=36:
+        chosen = "donkey"
         balance -= 1
     else:
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+        else:
+            chosen = "zebra"
         balance -= 0.5
-
+    print("you got a {}. Your balance is ${:.2f}".format(chosen, balance))
 
 print()
 print("Starting Balance: ${:.2f}".format(STARTING_BALANCE))
